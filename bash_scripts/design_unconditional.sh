ba016aae8d2e7a8c6a1e0cd86b8418503b3e7eee
A-Yarrow <yarrowmadrona@gmail.com>
2024-10-14 14:21:57 -0700

    updated design_unconditional.sh with env variables

--- bash_scripts/design_unconditional.sh
+++ bash_scripts/design_unconditional.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-# Here, we're making some unconditional designs
-# We specify the path for the outputs
-# We tell RFdiffusion that designs should be 100-200 residues in length (randomly sampled each design)
-# We generate 10 such designs
+
+AMINO_ACID_RANGE='[350-350]'
+NUM_DESIGNS=10
+OUTPUT_PREFIX=$HOME/outputs/unconditional
 
 cd $HOME/RFdiffusion
-HYDRA_FULL_ERROR=1
+
+# Initialize Conda for bash shell
+source $HOME/anaconda/etc/profile.d/conda.sh
+
+# Now activate the environment
+conda activate SE3nv
+
 docker run -it --rm --gpus all \
   -v $HOME/models:$HOME/models \
   -v $HOME/inputs:$HOME/inputs \
-  -v $HOME/outputs:$HOME/outputs \
   -v /root/RFdiffusion/examples/input_pdbs:/root/RFdiffusion/examples/input_pdbs \
   rfdiffusion \
-  inference.output_prefix=$HOME/outputs/unconditional \
+  inference.output_prefix=${OUTPUT_PREFIX} \
   inference.model_directory_path=$HOME/models \
   inference.input_pdb=/root/RFdiffusion/examples/input_pdbs/1qys.pdb \
-  inference.num_designs=10 \
-  'contigmap.contigs=[150-150]'
+  inference.num_designs=${NUM_DESIGNS} \
+  'contigmap.contigs='${AMINO_ACID_RANGE}
+
+cd $HOME
