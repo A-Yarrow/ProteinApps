db0046f49ed29896184b1b0780e9ed6df5c3955c
A-Yarrow <yarrowmadrona@gmail.com>
2024-11-08 17:36:57 -0800

    Added snakemake and test pdb to test direcotry

--- bash_scripts/full_install.sh
+++ bash_scripts/full_install.sh
@@ -1,4 +1,61 @@
 #!/bin/bash
+set -x
+
+#Clone the RFdiffusion git repository
+git clone git@github.com:RosettaCommons/RFdiffusion.git $HOME/RFdiffusion
+
+#Check if the RFdiffusion directory exists
+if [ -d $HOME/RFdiffusion ]; then
+    echo "RFdiffusion directory exists"
+    cd RFdiffusion
+
+else
+    echo "The RFdiffusion repository was not properly cloned. Exiting the script"
+    exit 1
+fi
+
+# Initialize Conda for bash shell
+source $HOME/anaconda/etc/profile.d/conda.sh
+
+# Create conda environment
+conda env create -f $HOME/RFdiffusion/env/SE3nv.yml
+
+# Now activate the environment
+conda activate SE3nv
+
+echo "Environment activated: $CONDA_DEFAULT_ENV"
+
+#Fix a bug in the docker file. Does not work with numpy < 2
+#https://github.com/RosettaCommons/RFdiffusion/pull/249/commits/30ae39e38e8196564e54674bffc688640ed4e75e
+sed -i '43i \  numpy==1.26.4 \\' $HOME/RFdiffusion/docker/Dockerfile
+
+#Docker Build
+#Make sure you are in $HOME/RFdiffusion when you run the docker build command
+cd $HOME/RFdiffusion
+docker build -f $HOME/RFdiffusion/docker/Dockerfile -t rfdiffusion .
+
+mkdir $HOME/inputs $HOME/outputs $HOME/models
+bash $HOME/RFdiffusion/scripts/download_models.sh $HOME/models
+wget -P $HOME/inputs https://files.rcsb.org/view/5TPN.pdb
+
+#Wait for models to download and test docker container
+
+docker run -it --rm --gpus all \
+  -v $HOME/models:$HOME/models \
+  -v $HOME/inputs:$HOME/inputs \
+  -v $HOME/outputs:$HOME/outputs \
+  rfdiffusion \
+  inference.output_prefix=$HOME/outputs/motifscaffolding \
+  inference.model_directory_path=$HOME/models \
+  inference.input_pdb=$HOME/inputs/5TPN.pdb \
+  inference.num_designs=3 \
+  'contigmap.contigs=[10-40/A163-181/10-40]'
+
+cd $HOME
+echo "You Should see new models in $HOME/outputs/motifscaffolding"
+
+##################################################################
+#!/bin/bash
 #INSTALL PROTEINMPNN-FASTRELAX ENVIRONMENT
 
 INPUT_YML="$HOME/dl_binder_design/include/proteinmpnn_fastrelax.yml"
@@ -59,15 +116,13 @@ else
 fi
 
 Append path to Silent Tools. 
-#export PATH="$PATH:/root/dl_binder_design/include/silent_tools"
+export PATH="$PATH:/root/dl_binder_design/include/silent_tools"
 #export PYTHONPATH="$PYTHONPATH:$HOME/dl_binder_design/include/silent_tools"
 Add export at end of .bashrc file
 sed -i '$ a #Append path for Silent Tools'
 sed -i '$ a export PATH="$PATH:/root/dl_binder_design/include/silent_tools"' $HOME/.bashrc
 sed -i '$ a export PATH="$PATH:/root/dl_binder_design/mpnn_fr"' $HOME/.bashrc
 sed -i '$ a export PATH="$PATH:/root/dl_binder_design/af2_initial_guess"' $HOME/.bashrc
-
-#sed -i '$ a export PYTHONPATH="$PYTHONPATH:$HOME/dl_binder_design/include/silent_tools"' $HOME/.bashrc
 source $HOME/.bashrc
 
 
@@ -83,7 +138,6 @@ conda env create -f $HOME/dl_binder_design/include/af2_binder_design.yml
 
 #Activate the AF2 conda environment
 conda activate af2_binder_design
-
 
 #Run install tests
 python $HOME/dl_binder_design/include/importtests/af2_importtest.py
@@ -103,5 +157,9 @@ else
 
 fi
 
+#Allow tensorflow to work more efficiently by using AutoGraph to convert python functions to Tensor Graphs
+pip install flax
+#Install Snakemake
+pip install snakemake
 #Test Silent Tools
 #silentfrompdbs pdb_test/*.pdb > test.silent
--- bash_scripts/mv_scripts.sh
+++ bash_scripts/mv_scripts.sh
@@ -3,13 +3,13 @@ PRIVATE_KEY='id_rsa'
 PUBLIC_KEY='id_rsa.pub'
 INSTALL_FILE='install-test.sh'
 RUN_FILE='design_protein_binders.sh'
-PDB_FILE='/home/yarrow/projects/proteinApps/outputs/protein-binders_0.pdb'
+PDB_FILE='/home/yarrow/projects/proteinApps/outputs/pdb_test'
 BINDER_INSTALL_FILE='install_dl_binder_design.sh'
 
 scp ${PRIVATE_KEY} ${PUBLIC_KEY} protein:/root/.ssh
 #scp ${INSTALL_FILE} protein:/root
 #scp ${RUN_FILE} protein:/root
-scp ${PDB_FILE} ${BINDER_INSTALL_FILE}  protein:/root
+scp -r ${PDB_FILE} ${BINDER_INSTALL_FILE}  protein:/root
 #scp ${YML_FILE} protein:/root
 
 
